2ecc9078d3849390b77723509d8f0327de2fd780
Olivier <yves.olli@gmail.com>
2014-07-07 21:31:43 +0200

    taoshan: don't force the inheritance of taoshan-vendor.mk

--- taoshan.mk
+++ taoshan.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, vendor/sony/taoshan/taoshan-vendor.mk)
+$(call inherit-product-if-exists, vendor/sony/taoshan/taoshan-vendor.mk)
 $(call inherit-product, device/sony/qcom-common/qcom-common.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
